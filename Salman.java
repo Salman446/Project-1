05d33bf8bea1c4e67940a2541737c816a2b72bd6
Salman446 <pathan333@live.com>
2019-02-04 21:24:01 +0530

    test trigger

--- Salman.java
+++ Salman.java
@@ -1,5 +1,5 @@
 public class Salman {
     public static void main(String[] args) {
-        System.out.println("Hello, Salman!");
+        System.out.println("Hello, this is complied from Jenkins-Github Webhook trigeer!");
     }
 }
